f4b1d08819dffb64559b54dc4493b5574b652994
Maximilian Kürschner <maximilian.kuerschner@futureself.education>
2022-10-28 09:52:55 +0200

    fix: remove main from install.mjs

--- bin/install.mjs
+++ bin/install.mjs
@@ -22,46 +22,43 @@ const projectPath = path.join(currentPath, projectName);
 // change to your boilerplate repo
 const git_repo = "https://github.com/programonaut/simple-ts-app.git";
 
-async function main() {
-  if (fs.existsSync(projectPath)) {
-    console.log(`The file ${projectName} already exist in the current directory, please give it another name.`);
-    process.exit(1);
-  }
-  else {
-    fs.mkdirSync(projectPath);
-  }
-
-  try {
-    const gitSpinner = ora("Downloading files...").start();
-    // clone the repo into the project folder -> creates the new boilerplate
-    await exec(`git clone --depth 1 ${git_repo} ${projectPath} --quiet`);
-    gitSpinner.succeed();
-
-    const cleanSpinner = ora("Removing useless files").start();
-    // remove my git history
-    const rmGit = rm(path.join(projectPath, ".git"), { recursive: true, force: true });
-    // remove the installation file
-    const rmBin = rm(path.join(projectPath, "bin"), { recursive: true, force: true });
-    await Promise.all([rmGit, rmBin]);
-
-    process.chdir(projectPath);
-    // remove the packages needed for cli
-    await exec("npm uninstall ora cli-spinners");
-    cleanSpinner.succeed();
-
-    const npmSpinner = ora("Installing dependencies...").start();
-    await exec("npm install");
-    npmSpinner.succeed();
-
-    console.log("The installation is done!");
-    console.log("You can now run your app with:");
-    console.log(`    cd ${projectName}`);
-    console.log(`    npm run dev`);
-
-  } catch (error) {
-    // clean up in case of error, so the user does not have to do it manually
-    fs.rmSync(projectPath, { recursive: true, force: true });
-    console.log(error);
-  }
+if (fs.existsSync(projectPath)) {
+  console.log(`The file ${projectName} already exist in the current directory, please give it another name.`);
+  process.exit(1);
 }
-main()
+else {
+  fs.mkdirSync(projectPath);
+}
+
+try {
+  const gitSpinner = ora("Downloading files...").start();
+  // clone the repo into the project folder -> creates the new boilerplate
+  await exec(`git clone --depth 1 ${git_repo} ${projectPath} --quiet`);
+  gitSpinner.succeed();
+
+  const cleanSpinner = ora("Removing useless files").start();
+  // remove my git history
+  const rmGit = rm(path.join(projectPath, ".git"), { recursive: true, force: true });
+  // remove the installation file
+  const rmBin = rm(path.join(projectPath, "bin"), { recursive: true, force: true });
+  await Promise.all([rmGit, rmBin]);
+
+  process.chdir(projectPath);
+  // remove the packages needed for cli
+  await exec("npm uninstall ora cli-spinners");
+  cleanSpinner.succeed();
+
+  const npmSpinner = ora("Installing dependencies...").start();
+  await exec("npm install");
+  npmSpinner.succeed();
+
+  console.log("The installation is done!");
+  console.log("You can now run your app with:");
+  console.log(`    cd ${projectName}`);
+  console.log(`    npm run dev`);
+
+} catch (error) {
+  // clean up in case of error, so the user does not have to do it manually
+  fs.rmSync(projectPath, { recursive: true, force: true });
+  console.log(error);
+}
